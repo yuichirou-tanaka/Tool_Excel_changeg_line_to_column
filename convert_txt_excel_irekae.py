e3bfa0b0c244501583a49bfb5d87428c373ba0e2
ebrtanakay <ebr.tanaka.y@gmail.com>
2022-03-16 13:27:42 +0900

    count 7

--- convert_txt_excel_irekae.py
+++ convert_txt_excel_irekae.py
@@ -1,15 +1,17 @@
 import sys
 
 args = sys.argv
-if len(args) <= 3:
+if len(args) <= 4:
     print("argument 引数3未満.")
     file_input_name = "test.txt"
     file_output_name = "out.csv"
+    count_retu = 7
     #print(args)
     #exit()
 else:
     file_input_name = args[1]
     file_output_name = args[2]
+    count_retu = args[3]
 
 # ----------------
 # first pin volt setting
@@ -25,7 +27,7 @@ with open(file_output_name, 'w') as f:
     
     while len(sall_line) > 0:
         fs = []
-        for i in range(3):
+        for i in range(count_retu):
             if len(sall_line) <= 0: 
                 break
             s = sall_line.pop(0)
