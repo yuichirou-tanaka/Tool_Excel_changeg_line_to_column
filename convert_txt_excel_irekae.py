f4d7f1e416c449ddadafaa85a0ee607b7639326d
ebrtanakay <ebr.tanaka.y@gmail.com>
2022-03-16 11:58:21 +0900

    python tools

---
+++ convert_txt_excel_irekae.py
@@ -0,0 +1,37 @@
+import sys
+
+args = sys.argv
+if len(args) <= 3:
+    print("argument 引数3未満.")
+    file_input_name = "test.txt"
+    file_output_name = "out.csv"
+    #print(args)
+    #exit()
+else:
+    file_input_name = args[1]
+    file_output_name = args[2]
+
+# ----------------
+# first pin volt setting
+#-----------------
+sall_line = []
+with open(file_input_name, 'r') as f: 
+    sall_line = f.readlines()
+
+#-----------------
+# CSV 出力
+#-----------------
+with open(file_output_name, 'w') as f: 
+    
+    while len(sall_line) > 0:
+        fs = []
+        for i in range(3):
+            if len(sall_line) <= 0: 
+                break
+            s = sall_line.pop(0)
+            s = s.replace("\n","")
+            #fs.append(s)
+            f.write(s + ",")
+        f.write("\n")
+        #f.write()
+
